a0914c2494023018836d9d305ccc95bfbfd04e92
dee-diaz <diazthediablo@yahoo.com>
2025-11-21 08:49:31 -0300

    Fix eslint config: prettier endOfLine auto

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,11 +5,22 @@ import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default defineConfig([
   globalIgnores(['webpack.*.js']),
+
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
     languageOptions: { globals: globals.browser },
+
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
   },
+
   eslintPluginPrettier,
 ]);
